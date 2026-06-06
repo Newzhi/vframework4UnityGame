a12448a33be16294efef5d766698c262a872d725
Newzhi <2330673541@qq.com>
2026-06-06 20:41:30 +0800

    资源模块学习

---
+++ vframework/Assets/vFramework/BaseLayer/AssetLayer/Impt/AssetLoader/ABundleLoader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABundleManager : IABundleManager
+{
+    public void Initialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Load()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void LoadAscyn()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void LoadWithCallBack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Unload()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ vframework/Assets/vFramework/BaseLayer/AssetLayer/InterFace/IAssetLoader/IABundleLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IABundleManager
+{
+    //初始化
+    public void Initialize();
+    
+    //同步加载资源
+    public void Load();
+    
+    //异步加载资源
+    public void LoadAscyn();
+    
+    //完成带回调
+    public void LoadWithCallBack();
+    
+    //卸载资源
+    public void Unload();
+}
